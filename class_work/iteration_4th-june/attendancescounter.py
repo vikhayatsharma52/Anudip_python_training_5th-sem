7aeccbb7b2bb86b29e5ab1cd1fdab353fa5b0803
vikhayatsharma52 <sharmavikhyat52@gmail.com>
2026-06-04 11:59:39 +0530

    Create attendancescounter.py

---
+++ class_work/iteration_4th-june/attendancescounter.py
@@ -0,0 +1,11 @@
+# Attendance Counter
+
+attendance = 0
+
+while attendance < 30:
+    print("Student Entered")
+    attendance = attendance + 1
+    print("Attendance Count:", attendance)
+    print()
+
+print("All students are present.")
