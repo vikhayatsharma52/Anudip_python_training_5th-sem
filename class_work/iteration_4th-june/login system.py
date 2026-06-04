ffafa8ebed4193e783d03dcaeedebf56c385648f
vikhayatsharma52 <sharmavikhyat52@gmail.com>
2026-06-04 12:09:38 +0530

    Create login system.py

---
+++ class_work/iteration_4th-june/login system.py
@@ -0,0 +1,7 @@
+password = ""
+
+while password != "admin123":
+    #enter the password
+    password = input("Enter password: ")
+#.................................................
+print("Login Successful!")
