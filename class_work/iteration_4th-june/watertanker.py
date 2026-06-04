f7c2b7072a9d6719f858f2d3dcea6b74c76e4769
vikhayatsharma52 <sharmavikhyat52@gmail.com>
2026-06-04 11:43:46 +0530

    Create watertanker.py

---
+++ class_work/iteration_4th-june/watertanker.py
@@ -0,0 +1,10 @@
+# Write a program that displays the amount of water in the tank after each minute and
+#  continues until the tank reaches 100 liters. 
+
+water = 0
+
+while water < 100:
+    water = water + 10
+    print("Water Level:", water, "liters")
+
+print(" the tank arefully ")
