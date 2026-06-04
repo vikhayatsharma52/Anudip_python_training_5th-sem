e469d3c3a1d1569fd5a0e50b10ac4794d3a76e82
vikhayatsharma52 <sharmavikhyat52@gmail.com>
2026-06-04 13:47:41 +0530

    Create Guessthegamenumber.py

---
+++ class_work/iteration_4th-june/Guessthegamenumber.py
@@ -0,0 +1,15 @@
+#  PROBLEM STATEMENT : program  Guess the Number Game 
+# Initialize guess with a value other than 7
+guess = 0
+# Repeat until the correct number is guessed
+while guess != 7:
+
+    # Take input from the user
+    guess = int(input("Guess the Number: "))
+
+    # Check if the guess is wrong
+    if guess != 7:
+        print("Wrong Guess. Try Again.")
+
+# Display success message when guess is correct
+print("Congratulations! You guessed the correct number.")
